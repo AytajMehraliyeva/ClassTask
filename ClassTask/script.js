e277408068dfa851b9e914ae8cb641b022f86e58
AytajMehraliyeva <145776968+AytajMehraliyeva@users.noreply.github.com>
2023-10-19 18:09:17 +0400

    Update script.js

--- ClassTask/script.js
+++ ClassTask/script.js
@@ -40,10 +40,12 @@ class Car {
 
 
 car1 = new Car("Kia", "Picanto", "Black", "2022", 180, 55, 200, 0.2, 40)
+car2 = new Car("Kia", "Sorento", "Gray", "2022", 120, 65, 300, 0.1, 50)
 console.log(car1.ShowInfo())
+console.log(car2.ShowInfo())
 
 console.log(car1.AddFuel(20))
-console.log(car1.AddFuel(10))
+console.log(car2.AddFuel(10))
 
 console.log(car1.Drive(400))
-console.log(car1.Drive(200))
+console.log(car2.Drive(200))
